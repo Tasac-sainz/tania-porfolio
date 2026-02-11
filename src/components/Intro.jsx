1bb12f7fdbc18f09cf7fa98faf464b3fa7c05271
Tania S. Sainz de Aja <tssainzdeaja@gmail.com>
2026-02-11 17:55:45 +0100

    add photo profile, add about and new css changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,23 @@
 import "react";
+import { Link } from "react-router-dom";
 
 const Intro = () => {
     return (
         <div className="intro_div">
             Reinventarse es volver a nacer pero con más sabiduría. <br></br>
+            <br></br>
             En Fenix Web Solutions creamos la web perfecta para tí con las
             tecnologías más modernas y que mejor se adaptan a tu idea. <br></br>
-            Echa un ojo a los proyectos y contacta.
+            <br></br>
+            Echa un ojo a los{" "}
+            <Link to="/projects" className="text-link">
+                proyectos
+            </Link>{" "}
+            y{" "}
+            <Link to="/contact" className="text-link">
+                contacta
+            </Link>
+            .{" "}
         </div>
     );
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,44 @@
 import "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Photo from "../images/photoexam.png";
 import "../styles/app.css";
 
 const About = () => {
     return (
         <>
             <Header />
-            <div>Sobre mí</div>
+            <div className="about-content">
+                <div className="photo">
+                    <img src={Photo} alt="profile photo" />
+                </div>
+                <div className="my-descr">
+                    ¡Hola! Soy Tania, la responsable de Fénix Web Solutions.{" "}
+                    <br></br>
+                    Este proyecto nace de mi pasión por el desarrollo web, el
+                    diseño y el márketing digital; además, soy psicóloga desde
+                    hace 15 años por lo que sé bien cómo llegar a tus
+                    clientes... <br></br>
+                    <br></br>
+                    Manejo tecnologías modernas dentro del universo tech (React,
+                    JavaScript, Flexbox...) para asegurarnos de que tu web
+                    además de atractiva, tenga todas las funcionalidades que
+                    necesitas. <br></br>
+                    <br></br>
+                    Visita{" "}
+                    <Link to="/projects" className="descrip-link">
+                        mis proyectos
+                    </Link>{" "}
+                    y verás todo lo que podemos hacer; Tú pones la idea y yo, tu
+                    web.
+                    <br></br>
+                    <br></br>
+                    <Link to="/contact" className="text-link">
+                        ¿Hablamos?
+                    </Link>
+                </div>
+            </div>
             <Footer />
         </>
     );
