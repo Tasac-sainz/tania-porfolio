40e90eabd0676e1c6bc939172042ce629bcc8556
Tania S. Sainz de Aja <tssainzdeaja@gmail.com>
2026-02-19 17:47:33 +0100

    New technology icons are added, progress in the array of projects within the ListProjects component and its layout

--- src/components/ListProjects.jsx
+++ src/components/ListProjects.jsx
@@ -5,6 +5,12 @@ import ApiIcon from "../images/api-icon.png";
 import SassIcon from "../images/sass-icon.png";
 import FlexIcon from "../images/flexbox-icon.png";
 import GitIcon from "../images/github-icon.png";
+import Node from "../images/node-icon.png";
+import Postman from "../images/postman-icon.png";
+import React from "../images/react-icon.png";
+import CSSicon from "../images/css-icon.png";
+import MySQL from "../images/mysql-icon.png";
+
 import "../styles/listprojects.css";
 
 const ListProjects = () => {
@@ -14,13 +20,18 @@ const ListProjects = () => {
         sass: SassIcon,
         flex: FlexIcon,
         git: GitIcon,
+        react: React,
+        node: Node,
+        postman: Postman,
+        css: CSSicon,
+        mysql: MySQL,
     };
 
     const projects = [
         {
             nameproj: "All for You",
             description:
-                "Web de ecommerce con diseño responsive, apto para el uso de distintos dispositivos.",
+                "Web de ecommerce desarrollada con JavaScript puro con diseño responsive, apto para el uso de distintos dispositivos.",
             features:
                 "Productos de API Rest, opción de búsqueda y reset, gestión completa de carrrito de compra (añadir, aumentar, disminuir, vaciar...), suma de importe total, almacenamiento local del cliente...",
             techs: ["js", "api", "sass", "flex", "git"],
@@ -29,7 +40,20 @@ const ListProjects = () => {
         {
             nameproj: "Recetas Four for Love",
             description:
-                "Servicio para que usuarios autenticados puedan crear y consultar sus propias recetas de cocina desde cualquier lugar del mundo",
+                "Web con recetas familiares en las que los usuarios pueden registrarse, añadir nuevas recetas y consultar sus propias recetas desde cualquier lugar del mundo. Desarrollada con React y NodeJS.",
+            features:
+                "Registro e inicio de sesión, creacción y consulta de recetas, almacenamiento local de usuario.",
+            techs: ["react", "node", "postman", "mysql", "css", "git"],
+            web: "https://github.com/Tasac-sainz/recetas-four-for-love.git",
+        },
+        {
+            nameproj: "Cool Projects",
+            description:
+                "Proyecto Full Stack para publicar proyectos de desarrollo web y darles visibilidad añadiendo imágenes, tecnologías, descripción y datos de autoría, desarrollada con React y NodeJS, despliegue con Render.com",
+            features:
+                "Publicación de proyecto, consulta de listado de proyectos, vista de detalle con enlaces a la demo del proyecto y al perfil de GitHub de su autor/a.",
+            techs: ["react", "node", "postman", "mysql", "css", "git"],
+            web: "https://project-promo-55-module-4-team-1.onrender.com", //no funciona el enlace de proyectos//
         },
     ];
     return (
